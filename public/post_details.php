e89a6abe9006384598284c3b41b643589b7db6e7
Farhan Rahman Arnob <farhanarnob@live.com>
2017-01-04 04:52:08 +0600

    post details updated.

--- public/post_details.php
+++ public/post_details.php
@@ -14,7 +14,32 @@ require_once('../include/connection.php');
 
 
 	
-
+<?php 
+    if($_GET["ID"]!=""){
+        $ID = $_GET["ID"];
+        $sql = "SELECT post_head,post_body, image FROM blogpost where ID=".$ID;
+        $result = mysqli_query($conn, $sql);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+            $image=$row["image"];
+            if($image == "image/"){
+            	$image = "image/1.jpg";
+            }
+            $post_head=$row["post_head"];
+            $post_body=$row["post_body"];
+            echo '<div class="blog-post clearfix">
+			<div class="blog-imgfull">
+			<img src="'.$image.'" alt="image">	
+			</div>
+			<div class="picture_blog-description ">'.
+			'<h2>'.$post_head.'</h2><br><hr>'.
+			'<p>'.$post_body.'</p></div><br><br>';
+	        }
+	    }
+	}else {
+            echo "0 results";
+    }
+ ?>
 	<!-- <div class="blog-post clearfix">
 		<div class="blog-imgfull">
 			<img src="image/1.jpg" alt="image">	
